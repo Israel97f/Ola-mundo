e83b73cd2bc1f9b34ebbe465e744426d2ac8b5dc
Israel Farias <israelfarias97@gmail.com>
2022-06-14 22:36:31 -0300

    Create loja.lua

---
+++ Lua/016/loja.lua
@@ -0,0 +1,13 @@
+--[[
+    Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados 
+    da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadra-
+    dos e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00.
+    Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total. 
+]]
+
+ -- Pede a área a ser pintada
+ io.write("Digite a área da superfície a ser pintada: ")
+ Area = tonumber(io.read())
+
+ -- processa os dados
+ 
