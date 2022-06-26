4e3b1841ce5fd18573c2b1178a731fdbb6f65702
Israel Farias <israelfarias97@gmail.com>
2022-06-25 21:07:48 -0300

    Update impa.lua

--- Lua/040/impa.lua
+++ Lua/040/impa.lua
@@ -3,3 +3,20 @@
     operador módulo (resto da divisão).
 ]]
 
+ -- pega um número
+ io.write("Digite um número: ")
+ Num = tonumber(io.read())
+
+ -- determina se o número é ímpar ou par
+ if Num % 1 == 0 then
+    if Num % 2 == 0 then
+        Mensagem = Num .. " é par"
+    else
+        Mensagem = Num .. " é ímpar"
+    end
+ else
+    Mensagem = Num .. " não é um numero inteiro"
+ end
+
+ -- exibe a mensagem
+ print(Mensagem)
