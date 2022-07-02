0bc32263a51f31009373b33fad9b8aaaf051178d
Israel Farias <israelfarias97@gmail.com>
2022-07-02 13:31:42 -0300

    Update população2.lua

--- Lua/051/população2.lua
+++ Lua/051/população2.lua
@@ -5,9 +5,16 @@
 
  -- pede ao usuário as informações necessárias
  while true do
-    io.write("Digite a população inicial da cidade A")
+    io.write("Digite a população inicial da cidade A: ")
     Populacao_A = tonumber(io.read())
-    io.write("Digite a população inicial de cidade B")
+    io.write("Digite a população inicial de cidade B: ")
     Populacao_B = tonumber(io.read())
+    io.write("Digite a taxa de crescimento da cidade A: ")
+    Taxa_A = tonumber(io.read())
+    io.write("Digite a taxa de crescimento da cidade B")
+    Taxa_B = tonumber(io.read())
 
+    if Populacao_A > 0 then
+      break
+    end
  end
