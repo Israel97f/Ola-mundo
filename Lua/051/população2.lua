fe63d9a3e177cee77b0a2b9b3ac300324c10feef
Israel Farias <israelfarias97@gmail.com>
2022-07-02 13:12:15 -0300

    Create população2.lua

---
+++ Lua/051/população2.lua
@@ -0,0 +1,13 @@
+ --[[
+    Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento
+    iniciais. Valide a entrada e permita repetir a operação. 
+ ]]
+
+ -- pede ao usuário as informações necessárias
+ while true do
+    io.write("Digite a população inicial da cidade A")
+    Populacao_A = tonumber(io.read())
+    io.write("Digite a população inicial de cidade B")
+    Populacao_B = tonumber(io.read())
+
+ end
