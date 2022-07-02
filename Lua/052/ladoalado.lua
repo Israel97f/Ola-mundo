f9a143a65ebe093892fa29b0f686005ec7d5eed7
Israel Farias <israelfarias97@gmail.com>
2022-07-02 15:50:14 -0300

    Create ladoalado.lua

---
+++ Lua/052/ladoalado.lua
@@ -0,0 +1,11 @@
+ --[[
+    Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique 
+    o programa para que ele mostre os números um ao lado do outro. 
+]]
+
+ -- mostra os números de 1 a vinte
+ for i = 1, 20, 1 do
+    --print(i)
+    io.write(i .. " ")
+ end
+ print("\n")
