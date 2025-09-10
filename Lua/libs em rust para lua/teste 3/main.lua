5b282c9c7410b6e81361d1d8a6a1fd3a2026c879
Israel97cf <91397521+Israel97f@users.noreply.github.com>
2025-09-10 13:12:58 -0300

    update

--- Lua/libs em rust para lua/teste 3/main.lua
+++ Lua/libs em rust para lua/teste 3/main.lua
@@ -1,5 +1,6 @@
 local toca_som = require("toca_som")
 
-toca_som.rode(
-	"C:/Users/Israe/Music/Tema de abertura da série Smallville.mp3"--'/home/israel/Música/Mortticia - Running Up That Hill (A Deal With God) [Kate Bush Cover] (MUSIC VIDEO).mp3'
-	)
+local path = os.getenv("OS") and "C:/Users/Israe/Music/Tema de abertura da série Smallville.mp3" or 
+'/home/israel/Música/Mortticia - Running Up That Hill (A Deal With God) [Kate Bush Cover] (MUSIC VIDEO).mp3'
+
+toca_som.rode(path)
