565d00a637d283253b7e4837e81237bcf5cf5a41
Israel97f <israelfarias97@gmail.com>
2025-09-29 01:36:35 -0300

    criado arquivo som.lua

---
+++ Lua/libs em rust para lua/teste 3/som.lua
@@ -0,0 +1,7 @@
+local toca_som = require("toca_som")
+
+local path = os.getenv("OS") and "C:/Users/Israe/Music/Tema de abertura da série Smallville.mp3" or 
+'/home/israel/Músicas/Mortticia - Running Up That Hill (A Deal With God) [Kate Bush Cover] (MUSIC VIDEO).mp3'
+
+
+toca_som.rode(path)
