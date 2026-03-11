91e02ca7b73573c8c7193993f78a1a9afbd98119
Israel97cf <91397521+Israel97f@users.noreply.github.com>
2026-03-10 23:29:11 -0300

    update

---
+++ Arduino/Projeto 2 - serial/radioControleSerial/radioControleSerial.ino
@@ -0,0 +1,75 @@
+// C++ code
+//
+
+int multiplier(int freq);
+void readButton ();
+int cont = 0;
+int tcnt1 = (0xFF << 8)|(0xE7);                  // 65511
+bool stbut = 0x00;
+int mult = 0x00;
+
+//*
+ISR(TIMER1_OVF_vect) {
+    cont ++;
+
+    if (cont >= mult){
+        cont = 0;
+        PORTB ^= (1 << DDB0);
+    } 
+  
+  	TCNT1 = tcnt1;
+}
+//*/
+
+void setup(){
+    Serial.begin(9600);                            // para comunicação serial
+    //*
+    cli();
+    TCCR1A = 0;                                   // Modo normal
+    TCCR1B = 0x03;//(1 << CS11) | (1 << CS10) ;   // Prescaler 64
+
+    TCNT1 = tcnt1;
+
+    TIMSK1 |= (1 << TOIE1);
+
+    sei();                                        // Ativa interrupção global
+    //*/
+
+    DDRB |= (1 << DDB0);                           // Configura PB0 como Saida
+    PORTB &= ~(1 << DDB0);
+
+    DDRD &= ~(DDD7) & ~(DDD6) & ~(DDD5) & ~(DDD4);
+    PORTD &= ~(DDD7) & ~(DDD6) & ~(DDD5) & ~(DDD4);
+}
+
+void loop(){
+  	readButton ();
+}
+
+int multiplier(int freq){
+  	int multiplier = (500 /freq) / 0.1 ;
+	return (multiplier);
+}
+
+void readButton (){
+    
+    if (!(PIND & (1 << DDD7))){  // frente
+     	Serial.write('a');
+    }
+
+    if (!(PIND & (1 << DDD6))){ // 1250 Hz
+     	mult = 4;
+    }
+
+    if (!(PIND & (1 << DDD5))){ // 500 Hz
+      	mult = 10;
+    }
+
+    if (!(PIND & (1 << DDD4))){ // ~ 714 Hz
+      	mult = 7;
+    }
+
+    if ((PIND & 0xF0) == 0xF0){ // 100 Hz
+     	mult = 50;
+    }
+}
---
+++ Arduino/Projeto 2 - serial/roboRadioControladoSerial/roboRadioControladoSerial.ino
@@ -0,0 +1,179 @@
+// C++ code
+//
+int adcCon(char ch);
+void muve(int velo, char dir);
+bool est = 0;
+float instante = 0;
+int freque = 0;
+int itera = 1;
+int duty = 0;
+bool primeraVez = 1;
+char comando = ' ';
+
+ISR (INT0_vect){ 
+	if(est){
+    freque = 1000000 / (micros() - instante);
+    est = !est;
+  } else {
+    instante = micros();
+    est = !est;
+  }
+  
+}
+
+void setup(){
+  //pinMode
+  DDRB = 0x37;
+  PORTB |= (1 << 5);
+
+  DDRD &= ~(1 << DDD2);  // entrada
+  PORTD |= (1 << DDD2);  // ativa pull up
+
+  DDRD |= (1 << DDD3);    //Pd3 como saida
+  PORTD &= ~(1 << DDD3);
+
+  // configurar PWM nas portas PB1 e PB2
+  TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11);  
+  TCCR1B = (1 << WGM12) | (1 << WGM13) | (1 << CS11);   // Prescaler = 8
+  ICR1 = 399; // define a frequecia do PWM
+
+  // configurar PWM PD3 e PB3
+  TCCR2A = 0b10100011;		 		  // Modo fast PWM, interrupção por comparação
+  TCCR2B = 0b00000101;				  // Prescaling em 1:1024
+  OCR2B = 255;                          // Valor entre 0 e 255 (50% de ciclo de trabalho)
+  // configura as interupções
+  EICRA |= (1 << ISC01) | (1 << ISC00); // Interrupção externa 0 como borda de subida
+  EIMSK |= (1 << INT0);                 // Interrupção externa 0 habilitada
+  sei();                                // liga as interupções globais
+
+  // Configura o covesor AD do atmega
+  ADMUX |= (1 << REFS0); // Seleciona atensão de referenci AVcc
+  ADCSRA |= (1 << ADEN) | (1 << ADIF) | (1 << ADPS1) | (1 << ADPS0); // Habilita o ADC e define o prescaler para 128
+
+  Serial.begin(9600);
+
+  while(1){
+    if (Serial.available() > 0) {
+      comando = Serial.read();
+      Serial.println(comando);
+    }
+
+      
+    if ((adcCon(0) >= 867) & (adcCon(2) >= 867)) {
+      if (comando == ' '){
+        muve(0, 's');
+      }
+      if (comando == 'a'){
+        muve(0, 'a');
+        comando = ' ';
+      }
+      if (freque > 1230 & freque <= 1270){ // frequencia de ~294 Hz estava sofrendo muita interferencia, não sei porque.
+        muve(0, 'b');
+      }
+      if (freque > 480 & freque <= 520){
+        muve(0, 'c');
+      }
+      if (freque > 694 & freque <= 734){
+        muve(0, 'd');
+      }
+      PORTB &= ~(1 << DDB5);
+    }else {
+      muve(10, 's');
+      PORTB |= (1 << DDB5);
+    }
+    delay(10);
+  }
+}
+
+void loop(){}
+
+int adcCon(char ch){
+	static int analogH, analogL, analog;
+  ADMUX =(ADMUX & 0xF8) | (ch & 0x07);
+  ADCSRA |= (1<<ADSC); 
+  while (!(ADCSRA & (1<<ADIF)));  // Aguarda a conversão terminar
+  ADCSRA |= (1<<ADIF);              // limpa a ADIF com trasição LOW para HIGH
+    
+  analogL = ADCL;
+  analogH = ADCH;
+    
+  analog = (analogH << 8) | analogL;
+
+  return analog;
+
+}
+
+void muve(int velo, char dir){
+
+  if(itera > 0){
+    if ((duty < ICR1) & (duty >= 0)){
+    duty = duty + itera;
+    } else {
+      if (duty >= ICR1){
+        duty = ICR1;
+      } else{
+        duty = 0;
+      }
+    }
+  } else {
+      if ((duty <= ICR1) & (duty > 0)){
+      duty = duty + itera;
+      } else {
+        if (duty >= ICR1){
+          duty = ICR1;
+        } else{
+          duty = 0;
+        }
+      }
+  }
+
+  OCR1A = duty;
+  OCR1B = duty;
+ 
+	switch (dir){
+    case 'a': 
+      // Frente
+      if (primeraVez) {duty = ICR1 * 0.75; primeraVez = 0;} 
+      PORTB &= ~(1 << 4);
+      //PORTB &= ~(1 << 2);
+      //PORTB |= (1 << 1);
+      PORTB &= ~(1 << 0);
+      itera = 1;
+      break;
+    case 'b':
+      // Ré
+      if (primeraVez) {duty = ICR1 * 0.5; primeraVez = 0;} 
+      PORTB |= (1 << 4);
+      //PORTB |= (1 << 2);
+      //PORTB &= ~(1 << 1);
+      PORTB |= (1 << 0);
+      itera = -1;
+      break;
+    case 'c':
+      // Direita
+      duty = ICR1 * 0.75;
+      PORTB &= ~(1 << 4);
+      //PORTB &= ~(1 << 2);
+      //PORTB &= ~(1 << 1);
+      PORTB |= (1 << 0);
+      break;
+    case 'd':
+      // Esquerda
+      duty = ICR1 * 0.75;
+      PORTB |= (1 << 4);
+      //PORTB |= (1 << 2);
+      //PORTB |= (1 << 1);
+      PORTB &= ~(1 << 0);
+      break;
+    case 's':
+      // Parar
+      PORTB &= ~(1 << 4);
+      //PORTB &= ~(1 << 2);
+      //PORTB &= ~(1 << 1);
+      PORTB &= ~(1 << 0);
+      duty = 0;
+      primeraVez = 1;
+      break;
+  	}
+
+}
