7a946897d400945bfdd2c7ff4a3fc05a5da4058a
Israel97cf <91397521+Israel97f@users.noreply.github.com>
2026-03-12 01:07:43 -0300

    update

--- Arduino/Projeto 2 - serial/roboRadioControladoSerial/roboRadioControladoSerial.ino
+++ Arduino/Projeto 2 - serial/roboRadioControladoSerial/roboRadioControladoSerial.ino
@@ -1,179 +1,36 @@
-// C++ code
-//
-int adcCon(char ch);
-void muve(int velo, char dir);
-bool est = 0;
-float instante = 0;
-int freque = 0;
-int itera = 1;
-int duty = 0;
-bool primeraVez = 1;
-char comando = ' ';
+#include <RH_ASK.h>
+#include <SPI.h>
 
-ISR (INT0_vect){ 
-	if(est){
-    freque = 1000000 / (micros() - instante);
-    est = !est;
-  } else {
-    instante = micros();
-    est = !est;
-  }
-  
-}
+RH_ASK driver;
 
-void setup(){
-  //pinMode
-  DDRB = 0x37;
-  PORTB |= (1 << 5);
+String comand = " ";
 
-  DDRD &= ~(1 << DDD2);  // entrada
-  PORTD |= (1 << DDD2);  // ativa pull up
-
-  DDRD |= (1 << DDD3);    //Pd3 como saida
-  PORTD &= ~(1 << DDD3);
-
-  // configurar PWM nas portas PB1 e PB2
-  TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11);  
-  TCCR1B = (1 << WGM12) | (1 << WGM13) | (1 << CS11);   // Prescaler = 8
-  ICR1 = 399; // define a frequecia do PWM
-
-  // configurar PWM PD3 e PB3
-  TCCR2A = 0b10100011;		 		  // Modo fast PWM, interrupção por comparação
-  TCCR2B = 0b00000101;				  // Prescaling em 1:1024
-  OCR2B = 255;                          // Valor entre 0 e 255 (50% de ciclo de trabalho)
-  // configura as interupções
-  EICRA |= (1 << ISC01) | (1 << ISC00); // Interrupção externa 0 como borda de subida
-  EIMSK |= (1 << INT0);                 // Interrupção externa 0 habilitada
-  sei();                                // liga as interupções globais
-
-  // Configura o covesor AD do atmega
-  ADMUX |= (1 << REFS0); // Seleciona atensão de referenci AVcc
-  ADCSRA |= (1 << ADEN) | (1 << ADIF) | (1 << ADPS1) | (1 << ADPS0); // Habilita o ADC e define o prescaler para 128
-
-  Serial.begin(9600);
-
-  while(1){
-    if (Serial.available() > 0) {
-      comando = Serial.read();
-      Serial.println(comando);
+void setup() {
+    Serial.begin(9600); // Para visualizar no Monitor Serial
+    if (!driver.init()) {
+        Serial.println("Erro ao iniciar receptor");
     }
 
-      
-    if ((adcCon(0) >= 867) & (adcCon(2) >= 867)) {
-      if (comando == ' '){
-        muve(0, 's');
-      }
-      if (comando == 'a'){
-        muve(0, 'a');
-        comando = ' ';
-      }
-      if (freque > 1230 & freque <= 1270){ // frequencia de ~294 Hz estava sofrendo muita interferencia, não sei porque.
-        muve(0, 'b');
-      }
-      if (freque > 480 & freque <= 520){
-        muve(0, 'c');
-      }
-      if (freque > 694 & freque <= 734){
-        muve(0, 'd');
-      }
-      PORTB &= ~(1 << DDB5);
-    }else {
-      muve(10, 's');
-      PORTB |= (1 << DDB5);
-    }
-    delay(10);
-  }
+    pinMode(13, OUTPUT);
 }
 
-void loop(){}
+void loop() {
+    uint8_t buf[12];   // Buffer para armazenar mensagem recebida
+    uint8_t buflen = sizeof(buf);
 
-int adcCon(char ch){
-	static int analogH, analogL, analog;
-  ADMUX =(ADMUX & 0xF8) | (ch & 0x07);
-  ADCSRA |= (1<<ADSC); 
-  while (!(ADCSRA & (1<<ADIF)));  // Aguarda a conversão terminar
-  ADCSRA |= (1<<ADIF);              // limpa a ADIF com trasição LOW para HIGH
-    
-  analogL = ADCL;
-  analogH = ADCH;
-    
-  analog = (analogH << 8) | analogL;
-
-  return analog;
-
-}
-
-void muve(int velo, char dir){
-
-  if(itera > 0){
-    if ((duty < ICR1) & (duty >= 0)){
-    duty = duty + itera;
-    } else {
-      if (duty >= ICR1){
-        duty = ICR1;
-      } else{
-        duty = 0;
-      }
+    if (driver.recv(buf, &buflen)) {
+        buf[buflen] = '\0'; // Finaliza string
+        Serial.print("Recebido: ");
+        Serial.println((char*)buf);
+        comand = (const char*) buf;
+        
     }
-  } else {
-      if ((duty <= ICR1) & (duty > 0)){
-      duty = duty + itera;
-      } else {
-        if (duty >= ICR1){
-          duty = ICR1;
-        } else{
-          duty = 0;
-        }
-      }
-  }
-
-  OCR1A = duty;
-  OCR1B = duty;
- 
-	switch (dir){
-    case 'a': 
-      // Frente
-      if (primeraVez) {duty = ICR1 * 0.75; primeraVez = 0;} 
-      PORTB &= ~(1 << 4);
-      //PORTB &= ~(1 << 2);
-      //PORTB |= (1 << 1);
-      PORTB &= ~(1 << 0);
-      itera = 1;
-      break;
-    case 'b':
-      // Ré
-      if (primeraVez) {duty = ICR1 * 0.5; primeraVez = 0;} 
-      PORTB |= (1 << 4);
-      //PORTB |= (1 << 2);
-      //PORTB &= ~(1 << 1);
-      PORTB |= (1 << 0);
-      itera = -1;
-      break;
-    case 'c':
-      // Direita
-      duty = ICR1 * 0.75;
-      PORTB &= ~(1 << 4);
-      //PORTB &= ~(1 << 2);
-      //PORTB &= ~(1 << 1);
-      PORTB |= (1 << 0);
-      break;
-    case 'd':
-      // Esquerda
-      duty = ICR1 * 0.75;
-      PORTB |= (1 << 4);
-      //PORTB |= (1 << 2);
-      //PORTB |= (1 << 1);
-      PORTB &= ~(1 << 0);
-      break;
-    case 's':
-      // Parar
-      PORTB &= ~(1 << 4);
-      //PORTB &= ~(1 << 2);
-      //PORTB &= ~(1 << 1);
-      PORTB &= ~(1 << 0);
-      duty = 0;
-      primeraVez = 1;
-      break;
-  	}
-
+    if (comand == "A1")
+    {
+        digitalWrite(13, 1);
+    }
+    else
+    {
+        digitalWrite(13, 0);
+    }
 }
